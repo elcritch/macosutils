5a9fa9a2682b94cd4b012df13d727fd9cc744084
Jaremy Creechley <creechley@gmail.com>
2025-01-15 15:19:27 -0700

    fixes

--- src/macosutils/cfcore.nim
+++ src/macosutils/cfcore.nim
@@ -13,65 +13,14 @@ type
 type
   CFRunLoopRef* = distinct pointer
   CFRunLoopMode* = distinct pointer
-  FSEventStreamRef* = distinct pointer
-  FSEventStreamEventId* = culonglong
   CFStringEncoding = uint32
-
-  # FSEventStreamEventFlags* = culong
-  dispatch_semaphore_t* = distinct pointer
-  
-  FSEventStreamCreateFlags* = enum
-    kFSEventStreamCreateFlagUseCFTypes
-    kFSEventStreamCreateFlagNoDefer
-    kFSEventStreamCreateFlagWatchRoot
-    kFSEventStreamCreateFlagIgnoreSelf
-    kFSEventStreamCreateFlagFileEvents
-    kFSEventStreamCreateFlagMarkSelf
-    kFSEventStreamCreateFlagFullHistory
-    kFSEventStreamCreateFlagUseExtendedData
-    kFSEventStreamCreateWithDocID
-
-  FSEventStreamEventFlags* = enum
-    kFSEventStreamEventFlagMustScanSubDirs
-    kFSEventStreamEventFlagUserDropped
-    kFSEventStreamEventFlagKernelDropped
-    kFSEventStreamEventFlagEventIdsWrapped
-    kFSEventStreamEventFlagHistoryDone
-    kFSEventStreamEventFlagRootChanged
-    kFSEventStreamEventFlagMount
-    kFSEventStreamEventFlagUnmount
-    kFSEventStreamEventFlagItemChangeOwner
-    kFSEventStreamEventFlagItemCreated
-    kFSEventStreamEventFlagItemFinderInfoMod
-    kFSEventStreamEventFlagItemInodeMetaMod
-    kFSEventStreamEventFlagItemIsDir
-    kFSEventStreamEventFlagItemIsFile
-    kFSEventStreamEventFlagItemIsHardlink
-    kFSEventStreamEventFlagItemIsLastHardlink
-    kFSEventStreamEventFlagItemIsSymlink
-    kFSEventStreamEventFlagItemModified
-    kFSEventStreamEventFlagItemRemoved
-    kFSEventStreamEventFlagItemRenamed
-    kFSEventStreamEventFlagItemXattrMod
-    kFSEventStreamEventFlagOwnEvent
-    kFSEventStreamEventFlagItemCloned
-
-  FSEventStreamContext* {.pure, final.} = object
-    version*: CFIndex
-    info*: pointer
-    retain*: pointer
-    release*: pointer
-    copyDescription*: pointer
 
 const
   kCFStringEncodingUTF8* = CFStringEncoding(0x08000100'u64)
   kCFRunLoopRunTimedOut* = 3
-  kFSEventStreamEventIdSinceNow* = 0xFFFFFFFFFFFFFFFF'u64
 
-  kFSEventStreamCreateFlagNone*: set[FSEventStreamCreateFlags] = {}
-  kFSEventStreamEventFlagNone*: set[FSEventStreamEventFlags] = {}
-  
-let kCFRunLoopDefaultMode* {.importc, extern: "kCFRunLoopDefaultMode".}: CFRunLoopMode
+let
+  kCFRunLoopDefaultMode* {.importc, extern: "kCFRunLoopDefaultMode".}: CFRunLoopMode
 
 # Core Foundation Functions
 proc CFStringCreateWithCString*(
@@ -173,36 +122,7 @@ proc createDefaultCFAllocator*(): CFAllocatorRef =
 
 # CoreFoundation Functions
 proc CFArrayCreate*(alloc: CFAllocatorRef, values: ptr pointer, numValues: CFIndex, callbacks: pointer): CFArrayRef {.importc.}
+
 proc CFRunLoopGetCurrent*(): CFRunLoopRef {.importc.}
 proc CFRunLoopRunInMode*(mode: CFRunLoopMode, seconds: CFTimeInterval, returnAfterSourceHandled: bool): cint {.importc.}
 proc CFRunLoopStop*(loop: CFRunLoopRef) {.importc.}
-
-# FSEvents Functions
-proc FSEventStreamCreate*(
-  allocator: CFAllocatorRef, 
-  callback: proc (
-    streamRef: FSEventStreamRef,
-    clientCallBackInfo: pointer,
-    numEvents: csize_t,
-    eventPaths: pointer,
-    # eventFlags: ptr set[FSEventStreamEventFlags],
-    eventFlags: UncheckedArray[set[FSEventStreamEventFlags]],
-    eventIds: UncheckedArray[FSEventStreamEventId],
-  ) {.cdecl.},
-  context: ptr FSEventStreamContext,
-  pathsToWatch: CFArrayRef,
-  sinceWhen: FSEventStreamEventId,
-  latency: CFTimeInterval,
-  flags: set[FSEventStreamCreateFlags]
-): FSEventStreamRef {.importc.}
-
-proc FSEventStreamScheduleWithRunLoop*(
-  streamRef: FSEventStreamRef,
-  runLoop: CFRunLoopRef,
-  runLoopMode: CFRunLoopMode
-) {.importc.}
-
-proc FSEventStreamStart*(streamRef: FSEventStreamRef): bool {.importc.}
-proc FSEventStreamStop*(streamRef: FSEventStreamRef) {.importc.}
-proc FSEventStreamInvalidate*(streamRef: FSEventStreamRef) {.importc.}
-proc FSEventStreamRelease*(streamRef: FSEventStreamRef) {.importc.}
---
+++ src/macosutils/fsstream.nim
@@ -0,0 +1,84 @@
+
+type
+  FSEventStreamRef* = distinct pointer
+  FSEventStreamEventId* = culonglong
+  FSEventStreamCreateFlags* = enum
+    kFSEventStreamCreateFlagUseCFTypes
+    kFSEventStreamCreateFlagNoDefer
+    kFSEventStreamCreateFlagWatchRoot
+    kFSEventStreamCreateFlagIgnoreSelf
+    kFSEventStreamCreateFlagFileEvents
+    kFSEventStreamCreateFlagMarkSelf
+    kFSEventStreamCreateFlagFullHistory
+    kFSEventStreamCreateFlagUseExtendedData
+    kFSEventStreamCreateWithDocID
+
+  FSEventStreamEventFlags* = enum
+    kFSEventStreamEventFlagMustScanSubDirs
+    kFSEventStreamEventFlagUserDropped
+    kFSEventStreamEventFlagKernelDropped
+    kFSEventStreamEventFlagEventIdsWrapped
+    kFSEventStreamEventFlagHistoryDone
+    kFSEventStreamEventFlagRootChanged
+    kFSEventStreamEventFlagMount
+    kFSEventStreamEventFlagUnmount
+    kFSEventStreamEventFlagItemChangeOwner
+    kFSEventStreamEventFlagItemCreated
+    kFSEventStreamEventFlagItemFinderInfoMod
+    kFSEventStreamEventFlagItemInodeMetaMod
+    kFSEventStreamEventFlagItemIsDir
+    kFSEventStreamEventFlagItemIsFile
+    kFSEventStreamEventFlagItemIsHardlink
+    kFSEventStreamEventFlagItemIsLastHardlink
+    kFSEventStreamEventFlagItemIsSymlink
+    kFSEventStreamEventFlagItemModified
+    kFSEventStreamEventFlagItemRemoved
+    kFSEventStreamEventFlagItemRenamed
+    kFSEventStreamEventFlagItemXattrMod
+    kFSEventStreamEventFlagOwnEvent
+    kFSEventStreamEventFlagItemCloned
+
+  FSEventStreamContext* {.pure, final.} = object
+    version*: CFIndex
+    info*: pointer
+    retain*: pointer
+    release*: pointer
+    copyDescription*: pointer
+
+type
+  kFSEventStreamEventIdSinceNow* = 0xFFFFFFFFFFFFFFFF'u64
+
+  kFSEventStreamCreateFlagNone*: set[FSEventStreamCreateFlags] = {}
+  kFSEventStreamEventFlagNone*: set[FSEventStreamEventFlags] = {}
+  
+let kCFRunLoopDefaultMode* {.importc, extern: "kCFRunLoopDefaultMode".}: CFRunLoopMode
+
+# FSEvents Functions
+proc FSEventStreamCreate*(
+  allocator: CFAllocatorRef, 
+  callback: proc (
+    streamRef: FSEventStreamRef,
+    clientCallBackInfo: pointer,
+    numEvents: csize_t,
+    eventPaths: pointer,
+    # eventFlags: ptr set[FSEventStreamEventFlags],
+    eventFlags: UncheckedArray[set[FSEventStreamEventFlags]],
+    eventIds: UncheckedArray[FSEventStreamEventId],
+  ) {.cdecl.},
+  context: ptr FSEventStreamContext,
+  pathsToWatch: CFArrayRef,
+  sinceWhen: FSEventStreamEventId,
+  latency: CFTimeInterval,
+  flags: set[FSEventStreamCreateFlags]
+): FSEventStreamRef {.importc.}
+
+proc FSEventStreamScheduleWithRunLoop*(
+  streamRef: FSEventStreamRef,
+  runLoop: CFRunLoopRef,
+  runLoopMode: CFRunLoopMode
+) {.importc.}
+
+proc FSEventStreamStart*(streamRef: FSEventStreamRef): bool {.importc.}
+proc FSEventStreamStop*(streamRef: FSEventStreamRef) {.importc.}
+proc FSEventStreamInvalidate*(streamRef: FSEventStreamRef) {.importc.}
+proc FSEventStreamRelease*(streamRef: FSEventStreamRef) {.importc.}
